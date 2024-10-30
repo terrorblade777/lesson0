8a915c3a5840322e5be0fb916e09642799127066
terrorblade777 <shwarzzy777@gmail.com>
2024-10-30 17:12:19 +0600

    Add files via upload

---
+++ main.py..py
@@ -0,0 +1,13 @@
+print("1st programs")
+print(9**0.5*5) # 2 вариант. По контексту не понял порядок выполнения
+
+print("2st programs")
+print(9.99>9.98!=1000.1)
+
+print("3st programs")
+print(2*(2+2))
+print(2*2+2)
+print(8==6)
+
+print("4st programs")
+print(int(123.456*10%10))
