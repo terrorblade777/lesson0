f7efd3127838a6422508bac9a3ee3c454edb6cc5
terrorblade777 <shwarzzy777@gmail.com>
2024-11-07 14:58:49 +0600

    Add files via upload
    Исправленный, я не добавил команду upper

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input("Привет, как тебя зовут? ")
+#print(my_string)
+print("Привет,",my_string.upper())
+print("my_string".upper())
+print("my_string".lower())
+print("my_string".replace("_",""))
+print("my_string"[1])
+print("my_string"[-1])
+
