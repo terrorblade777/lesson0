06dbd5204df7d539f2b8c789f80ab32a8d22cbd7
terrorblade777 <shwarzzy777@gmail.com>
2024-11-11 19:55:49 +0600

    Add files via upload

--- variables исправлены.py
+++ variables исправлены.py
@@ -1,12 +1,12 @@
 number_of_completed_remote_sensing=12
-print(number_of_completed_remote_sensing)
+#print(number_of_completed_remote_sensing)
 number_of_hours_spent = 1.5
-print(number_of_hours_spent)
+#print(number_of_hours_spent)
 course_name="Python"
-print(course_name)
+#print(course_name)
 time_per_task=(1.5/12)
-print(time_per_task)
-print("Курс:", course_name,"количество выполненых задач:",
+#print(time_per_task)
+print("Курс:", course_name,",", "количество выполненых задач:",
       number_of_completed_remote_sensing,",","затрачено часов:",
       number_of_completed_remote_sensing ,',',"среднее выполнение задания:",
       time_per_task, "часа.")
