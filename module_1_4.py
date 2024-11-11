1b01a4b590cf5e3bc6fe3e8e59b8e4db1cb8b096
terrorblade777 <shwarzzy777@gmail.com>
2024-11-11 19:45:54 +0600

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,10 +1,10 @@
 my_string =input("Привет, как тебя зовут? ")
 print(len(my_string))
 print(my_string.upper())
-print("Привет,",my_string,"!")
-print("my_string".upper())
-print("my_string".lower())
-print("my_string".replace("_",""))
-print("my_string"[1])
-print("my_string"[-1])
+#print("Привет,",my_string,"!")
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
+
 
